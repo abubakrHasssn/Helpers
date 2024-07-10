c1a82dcae105704b446e72aa8811ff91d37e4f3a
Abubakr Hassan <74971786+abubakrHasssn@users.noreply.github.com>
2024-07-11 01:47:46 +0400

    Add files via upload

---
+++ ExceptionHandlerMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo.WebApi.Common.DTOs.InfrastructureDTOs;
+using Demo.Domain.Abstract.Entities;
+
+namespace Demo.WebApi.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IloggerService _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, IloggerService logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                ErrorHandling error = new ErrorHandling
+                {
+                    StatusCode = context.Response.StatusCode.ToString(),
+                    ErrorMessage = ex.Message
+                };
+
+                await context.Response.WriteAsync(error.ToString());
+            }
+        }
+    }
+}
